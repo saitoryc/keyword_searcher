6e0ea595cfdd8f8d1464e2ee9f92b3e0bba2702a
ryoichi-saito <ryoichi_saito@waku-2.com>
2014-11-20 14:26:49 +0900

    テンプレ用Excelを追加 行数と一致した行を出力する機能を追加

--- insertCheck.rb
+++ insertCheck.rb
@@ -8,30 +8,31 @@ workbook = excel.workbooks.open({'filename'=> filename, 'readOnly' => false})
 ws = workbook.sheets[1]
 current_row=5
 
-while ws.rows[current_row].columns[4].value != "" do
+while ws.rows[current_row].columns[4].value != nil do
 	url = ws.rows[current_row].columns[4].value
 	searchWord = ws.rows[current_row].columns[5].value
 
-	sio = OpenURI.open_uri(url)
-	check = sio.read.include?(searchWord)
-	if check
-		ws.rows[current_row].columns[8].value = "OK"
-		ws.rows[current_row].Interior.ColorIndex = 43
-
-		lines = searchWord.split("¥n")
-		lineCnt = 1
-		for line in lines do
-			if (line.include?(searchWord))
-				ws.rows[current_row].columns[9].value = lineCnt
-				ws.rows[current_row].columns[10].value = line
-				break
-			end
-			lineCnt = lineCnt + 1
+	open(url) {|sio|
+		html = sio.read
+		check = html.include?(searchWord)
+		if check
+			ws.rows[current_row].columns[8].value = "OK"
+			ws.rows[current_row].Interior.ColorIndex = 43
+			
+			lineCnt = 1
+			html.each_line {|line|
+				if (line.include?(searchWord))
+					ws.rows[current_row].columns[10].value = lineCnt
+					ws.rows[current_row].columns[11].value = line.chomp
+					break
+				end
+				lineCnt = lineCnt + 1
+			}
+		else
+			ws.rows[current_row].columns[8].value = "NG"
+			ws.rows[current_row].Interior.ColorIndex = 3
 		end
-	else
-		ws.rows[current_row].columns[8].value = "NG"
-		ws.rows[current_row].Interior.ColorIndex = 3
-	end
+	}
 
 	current_row = current_row + 1
 end
